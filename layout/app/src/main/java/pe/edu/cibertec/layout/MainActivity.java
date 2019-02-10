c6b6cb4f5ae06751a2b082a9e007a437cafe10ab
ANDROID <kenny.i.carbajal@gmail.com>
2019-02-09 20:06:31 -0500

    asdad

--- layout/app/src/main/java/pe/edu/cibertec/layout/MainActivity.java
+++ layout/app/src/main/java/pe/edu/cibertec/layout/MainActivity.java
@@ -1,4 +1,4 @@
-package pe.edu.cibertec.layouts;
+package pe.edu.cibertec.layout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,11 +10,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
-
-
-
-
-
 }
